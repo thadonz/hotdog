10740bc6040e5b9b701e46cddfbe15d0ff463197
Don Dao <ddao@obsglobal.com>
2019-11-14 20:28:10 -0600

    style results dialog

--- src/components/resultDialog/ResultDialog.js
+++ src/components/resultDialog/ResultDialog.js
@@ -1,21 +1,44 @@
 import React, { PureComponent } from 'react';
-import {Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, Button} from '@material-ui/core';
+import {Dialog, DialogActions, DialogContent, Button, LinearProgress} from '@material-ui/core';
+import {CheckCircleOutline, HighlightOff} from '@material-ui/icons';
+import classNames from 'classnames';
+import './styles.css';
 
 class ResultDialog extends PureComponent {
     render() {
-        const {open, handleClose, imageToClassify} = this.props;
+        const {open, handleClose, imageToClassify, isHotdog, info} = this.props;
 
         return (
             <Dialog
                 open={open}
-                aria-labelledby="alert-dialog-title"
-                aria-describedby="alert-dialog-description"
             >
-                <DialogTitle id="alert-dialog-title">{"Results"}</DialogTitle>
-                <DialogContent>
-                    <DialogContentText id="alert-dialog-description">
-                    </DialogContentText>
-                    {imageToClassify && (<img src={imageToClassify} alt="preview" />)}
+                <DialogContent className='result-content'>
+                    {
+                        isHotdog != null 
+                            ? (
+                                <div className={classNames({success: isHotdog, error: !isHotdog})}>
+                                    <h2>{`${isHotdog ? '': 'not a '}hotdog`}</h2>
+                                    <div className='icon-wrapper'>
+                                        {
+                                            isHotdog ? <CheckCircleOutline fontSize='large' /> : <HighlightOff fontSize='large' />
+                                        }
+                                    </div>
+                                    <div className='classification-info'>
+                                        {
+                                            info.map(({className, probability}) => (
+                                                <div key={className}>
+                                                    <div className='classification'>{`classnames: ${className}`}</div>
+                                                    <div>{`probability: ${probability * 100}%`}</div>
+                                                </div>
+                                            ))
+                                        }
+                                    </div>
+                                </div>                
+
+                            )
+                            : (<LinearProgress />)
+                    }
+                    {imageToClassify && (<img src={imageToClassify} alt="classified" />)}
                 </DialogContent>
                 <DialogActions>
                     <Button onClick={handleClose} color="primary">
